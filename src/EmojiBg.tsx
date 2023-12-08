2c85fc4a8914722a8363da3875f1da31c464e46b
Turing <shspku365@gmail.com>
2023-12-08 07:44:46 +0000

    优化表情背景效果

--- src/EmojiBg.tsx
+++ src/EmojiBg.tsx
@@ -1,129 +1,15 @@
 // EmojiBg.tsx
-import React, { useEffect, useRef, useState } from "react";
-import "./EmojiBg.css";
+import React, { useEffect, useRef, useState } from 'react';
+import './EmojiBg.css';
 
 interface EmojiBgProps {
-  direction?: "horizontal" | "vertical";
+  direction?: 'horizontal' | 'vertical';
   emojiSize?: number; // 表情符号的大小（单位：像素）
 }
 
-const emojis = [
-  "😀",
-  "😃",
-  "😄",
-  "😁",
-  "😆",
-  "😅",
-  "😂",
-  "🤣",
-  "😊",
-  "😇",
-  "🙂",
-  "🙃",
-  "😉",
-  "😌",
-  "😍",
-  "🥰",
-  "😘",
-  "😗",
-  "😙",
-  "😚",
-  "😋",
-  "😛",
-  "😝",
-  "😜",
-  "🤪",
-  "🤨",
-  "🧐",
-  "🤓",
-  "😎",
-  "🤩",
-  "🥳",
-  "😏",
-  "😒",
-  "😞",
-  "😔",
-  "😟",
-  "😕",
-  "🙁",
-  "😣",
-  "😖",
-  "😫",
-  "😩",
-  "🥺",
-  "😢",
-  "😭",
-  "😤",
-  "😠",
-  "😡",
-  "🤬",
-  "🤯",
-  "😳",
-  "🥵",
-  "🥶",
-  "😱",
-  "😨",
-  "😰",
-  "😥",
-  "😓",
-  "🤗",
-  "🤔",
-  "🤭",
-  "🤫",
-  "🤥",
-  "😶",
-  "😐",
-  "😑",
-  "😬",
-  "🙄",
-  "😯",
-  "😦",
-  "😧",
-  "😮",
-  "😲",
-  "🥱",
-  "😴",
-  "🤤",
-  "😪",
-  "😵",
-  "🤐",
-  "🥴",
-  "🤢",
-  "🤮",
-  "🤧",
-  "😷",
-  "🤒",
-  "🤕",
-  "🤑",
-  "🤠",
-  "😈",
-  "👿",
-  "👹",
-  "👺",
-  "🤡",
-  "💩",
-  "👻",
-  "💀",
-  "☠️",
-  "👽",
-  "👾",
-  "🤖",
-  "🎃",
-  "😺",
-  "😸",
-  "😹",
-  "😻",
-  "😼",
-  "😽",
-  "🙀",
-  "😿",
-  "😾",
-];
+const emojis = ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰', '😘', '😗', '😙', '😚', '😋', '😛', '😝', '😜', '🤪', '🤨', '🧐', '🤓', '😎', '🤩', '🥳', '😏', '😒', '😞', '😔', '😟', '😕', '🙁', '😣', '😖', '😫', '😩', '🥺', '😢', '😭', '😤', '😠', '😡', '🤬', '🤯', '😳', '🥵', '🥶', '😱', '😨', '😰', '😥', '😓', '🤗', '🤔', '🤭', '🤫', '🤥', '😶', '😐', '😑', '😬', '🙄', '😯', '😦', '😧', '😮', '😲', '🥱', '😴', '🤤', '😪', '😵', '🤐', '🥴', '🤢', '🤮', '🤧', '😷', '🤒', '🤕', '🤑', '🤠', '😈', '👿', '👹', '👺', '🤡', '💩', '👻', '💀', '☠️', '👽', '👾', '🤖', '🎃', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '😾'];
 
-const EmojiBg: React.FC<EmojiBgProps> = ({
-  direction = "horizontal",
-  emojiSize = 32,
-}) => {
+const EmojiBg: React.FC<EmojiBgProps> = ({ direction = 'horizontal', emojiSize = 32 }) => {
   const scrollerRef = useRef<HTMLDivElement>(null);
   const [emojiGrid, setEmojiGrid] = useState<JSX.Element[]>([]);
   const [numRows, setNumRows] = useState(0);
@@ -136,16 +22,16 @@ const EmojiBg: React.FC<EmojiBgProps> = ({
       const screenHeight = window.innerHeight;
       const rows = Math.ceil(screenHeight / emojiSize);
       const cols = Math.ceil(screenWidth / emojiSize);
-
-      setNumRows(rows);
-      setNumCols(cols);
+      // 设置两倍的格子，防止滚动时出现空白
+      setNumRows(rows * 2);
+      setNumCols(cols * 2);
     };
 
     calculateGridSize(); // 初始计算
-    window.addEventListener("resize", calculateGridSize); // 调整大小时再次计算
+    window.addEventListener('resize', calculateGridSize); // 调整大小时再次计算
 
     return () => {
-      window.removeEventListener("resize", calculateGridSize);
+      window.removeEventListener('resize', calculateGridSize);
     };
   }, [emojiSize]);
 
@@ -155,14 +41,9 @@ const EmojiBg: React.FC<EmojiBgProps> = ({
       return Array.from({ length: numRows }, (_, rowIndex) => (
         <div key={`row-${rowIndex}`} className="emoji-row">
           {Array.from({ length: numCols }, (_, colIndex) => {
-            const randomEmoji =
-              emojis[Math.floor(Math.random() * emojis.length)];
+            const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
             return (
-              <span
-                key={`emoji-${rowIndex}-${colIndex}`}
-                className="emoji"
-                style={{ fontSize: `${emojiSize}px` }}
-              >
+              <span key={`emoji-${rowIndex}-${colIndex}`} className="emoji" style={{ fontSize: `${emojiSize}px` }}>
                 {randomEmoji}
               </span>
             );
@@ -175,23 +56,23 @@ const EmojiBg: React.FC<EmojiBgProps> = ({
   }, [numRows, numCols, emojiSize]);
 
   // 处理滚动动画
-  useEffect(() => {
-    const scroller = scrollerRef.current;
-    if (scroller) {
-      let position = 0;
-      const speed = 2; // 可调整滚动速度
-      const scroll = () => {
-        position += speed;
-        if (direction === "vertical") {
-          scroller.scrollTop = position;
-        } else {
-          scroller.scrollLeft = position;
-        }
-        requestAnimationFrame(scroll);
-      };
-      requestAnimationFrame(scroll);
-    }
-  }, [direction]);
+  // useEffect(() => {
+  //   const scroller = scrollerRef.current;
+  //   if (scroller) {
+  //     let position = 0;
+  //     const speed = 2; // 可调整滚动速度
+  //     const scroll = () => {
+  //       position += speed;
+  //       if (direction === 'vertical') {
+  //         scroller.scrollTop = position;
+  //       } else {
+  //         scroller.scrollLeft = position;
+  //       }
+  //       requestAnimationFrame(scroll);
+  //     };
+  //     requestAnimationFrame(scroll);
+  //   }
+  // }, [direction]);
 
   return (
     <div className={`emoji-scroller ${direction}`} ref={scrollerRef}>
