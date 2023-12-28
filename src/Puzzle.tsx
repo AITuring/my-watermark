6d429b52785415ae2a053129e3c6e3238a5cff8c
AITuring <shspku365@gmail.com>
2023-12-28 22:38:03 +0800

    拼图可以拖拽调整位置

--- src/Puzzle.tsx
+++ src/Puzzle.tsx
@@ -1,16 +1,129 @@
-import { useCallback, useState, useRef } from "react";
+import { forwardRef, useCallback, useState, useRef, memo } from "react";
 import { useDropzone } from "react-dropzone";
 import { useNavigate } from "react-router-dom";
 import { FloatButton, Spin, message, Button, Slider } from "antd";
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  UniqueIdentifier,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+} from "@dnd-kit/sortable";
 import { PictureFilled } from "@ant-design/icons";
-import { PhotoAlbum, RenderContainer } from "react-photo-album";
+import clsx from "clsx";
+import {
+  PhotoAlbum,
+  RenderContainer,
+  Photo,
+  RenderPhotoProps,
+} from "react-photo-album";
 import html2canvas from "html2canvas";
 import "./puzzle.css";
 
+interface SortablePhoto extends Photo {
+  id: UniqueIdentifier;
+}
+
+type SortablePhotoProps = RenderPhotoProps<SortablePhoto>;
+
+type PhotoFrameProps = SortablePhotoProps & {
+  overlay?: boolean;
+  active?: boolean;
+  insertPosition?: "before" | "after";
+  attributes?: Partial<React.HTMLAttributes<HTMLDivElement>>;
+  listeners?: Partial<React.HTMLAttributes<HTMLDivElement>>;
+};
 interface ImgProp {
+  id: string;
   src: string;
   width: number;
   height: number;
+}
+
+const PhotoFrame = memo(
+  forwardRef<HTMLDivElement, PhotoFrameProps>(function PhotoFrame(props, ref) {
+    const {
+      layoutOptions,
+      imageProps,
+      overlay,
+      active,
+      insertPosition,
+      attributes,
+      listeners,
+    } = props;
+    const { alt, style, ...restImageProps } = imageProps;
+
+    return (
+      <div
+        ref={ref}
+        style={{
+          width: overlay
+            ? `calc(100% - ${2 * layoutOptions.padding}px)`
+            : style.width,
+          padding: style.padding,
+          marginBottom: style.marginBottom,
+        }}
+        className={clsx("photo-frame", {
+          overlay: overlay,
+          active: active,
+          insertBefore: insertPosition === "before",
+          insertAfter: insertPosition === "after",
+        })}
+        {...attributes}
+        {...listeners}
+      >
+        <img
+          alt={alt}
+          style={{
+            ...style,
+            width: "100%",
+            height: "auto",
+            padding: 0,
+            marginBottom: 0,
+          }}
+          {...restImageProps}
+        />
+      </div>
+    );
+  })
+);
+
+function SortablePhotoFrame(
+  props: SortablePhotoProps & { activeIndex?: number }
+) {
+  const { photo, activeIndex } = props;
+  const { attributes, listeners, isDragging, index, over, setNodeRef } =
+    useSortable({ id: photo.id });
+
+  return (
+    <PhotoFrame
+      ref={setNodeRef}
+      active={isDragging}
+      insertPosition={
+        activeIndex !== undefined && over?.id === photo.id && !isDragging
+          ? index > activeIndex
+            ? "after"
+            : "before"
+          : undefined
+      }
+      aria-label="sortable image"
+      attributes={attributes}
+      listeners={listeners}
+      {...props}
+    />
+  );
 }
 
 const Puzzle = () => {
@@ -22,6 +135,46 @@ const Puzzle = () => {
   const [isUpload, setIsUpload] = useState<boolean>(false);
   const [inputColumns, setInputColumns] = useState<number>(3);
 
+  const renderedPhotos = useRef<{ [key: string]: SortablePhotoProps }>({});
+  const [activeId, setActiveId] = useState<UniqueIdentifier>();
+  const activeIndex = activeId
+    ? images.findIndex((photo) => photo.id === activeId)
+    : undefined;
+
+  const sensors = useSensors(
+    useSensor(MouseSensor, { activationConstraint: { distance: 5 } }),
+    useSensor(TouchSensor, {
+      activationConstraint: { delay: 50, tolerance: 10 },
+    }),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+  );
+
+  const handleDragStart = useCallback(
+    ({ active }: DragStartEvent) => setActiveId(active.id),
+    []
+  );
+
+  const handleDragEnd = useCallback((event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (over && active.id !== over.id) {
+      setImages((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+
+    setActiveId(undefined);
+  }, []);
+
+  const renderPhoto = (props: SortablePhotoProps) => {
+    // capture rendered photos for future use in DragOverlay
+    renderedPhotos.current[props.photo.id] = props;
+    return <SortablePhotoFrame activeIndex={activeIndex} {...props} />;
+  };
+
   const onDrop = useCallback((acceptedFiles) => {
     setFiles((prev) => [...prev, ...acceptedFiles]);
     acceptedFiles.forEach((file) => {
@@ -32,6 +185,7 @@ const Puzzle = () => {
         setImages((prev) => [
           ...prev,
           {
+            id: src,
             src: src,
             width: img.width,
             height: img.height,
@@ -41,7 +195,6 @@ const Puzzle = () => {
       setIsUpload(true);
     });
   }, []);
-
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
@@ -72,7 +225,7 @@ const Puzzle = () => {
           }
         },
         "image/jpeg",
-        0.9,
+        0.9
       );
     } else {
       const galleryElement = document.getElementById("container");
@@ -89,7 +242,7 @@ const Puzzle = () => {
           }
         },
         "image/jpeg",
-        0.9,
+        0.9
       );
     }
   };
@@ -137,14 +290,39 @@ const Puzzle = () => {
               </Button>
             </div>
           </div>
-          <PhotoAlbum
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          >
+            <SortableContext items={images}>
+              <div style={{ margin: 30 }}>
+                <PhotoAlbum
+                  layout="columns"
+                  photos={images}
+                  padding={0}
+                  spacing={0}
+                  columns={inputColumns}
+                  renderContainer={renderContainer}
+                  renderPhoto={renderPhoto}
+                />
+              </div>
+            </SortableContext>
+            <DragOverlay>
+              {activeId && (
+                <PhotoFrame overlay {...renderedPhotos.current[activeId]} />
+              )}
+            </DragOverlay>
+          </DndContext>
+          {/* <PhotoAlbum
             layout="columns"
             photos={images}
             padding={0}
             spacing={0}
             columns={inputColumns}
             renderContainer={renderContainer}
-          />
+          /> */}
         </div>
       ) : (
         <div className="upload">
@@ -154,7 +332,9 @@ const Puzzle = () => {
             className="bg"
           />
           <input {...getInputProps()} />
-          <div {...getRootProps()} className="upload-button">选择（或拖拽）图片</div>
+          <div {...getRootProps()} className="upload-button">
+            选择（或拖拽）图片
+          </div>
         </div>
       )}
       <FloatButton
