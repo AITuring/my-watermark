69c5448abdb49389a88c22a86e1a4081738ea58f
AITuring <shspku365@gmail.com>
2024-07-12 13:36:37 +0800

    perf: vite分包优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,44 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        viteCompression({
+            verbose: true, // 是否在控制台中输出压缩结果
+            disable: false,
+            threshold: 10240, // 如果体积大于阈值，将被压缩，单位为b，体积过小时请不要压缩，以免适得其反
+            algorithm: "gzip", // 压缩算法，可选['gzip'，' brotliccompress '，'deflate '，'deflateRaw']
+            ext: ".gz",
+            deleteOriginFile: true, // 源文件压缩后是否删除(我为了看压缩后的效果，先选择了true)
+        }),
+        visualizer({ open: true }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    } else {
+                        return "common";
+                    }
+                },
+            },
+        },
+        terserOptions: {
+            compress: {
+                drop_console: true, // 生产环境自动删除console
+                pure_funcs: ["console.log"], // 压缩的时候删除所有的console
+            },
+        },
+    },
 });
