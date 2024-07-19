02da7f5d38adb9421205f0d8f8b2f29cf06ff942
AITuring <shspku365@gmail.com>
2024-07-19 09:43:30 +0800

    perf:删除antd icons引入

--- src/WatermarkEditor.tsx
+++ src/WatermarkEditor.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/display-name */
 import React, { useState, useEffect, useRef, forwardRef } from "react";
-// import { button } from "antd";
 import Konva from "konva";
 import { Stage, Layer, Image as KonvaImage, Transformer } from "react-konva";
 import useImage from "use-image";
