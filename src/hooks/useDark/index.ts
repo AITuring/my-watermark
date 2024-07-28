688ac8f00c030b244e24f1dddb212a565bc0d5b2
AITuring <shspku365@gmail.com>
2024-07-29 02:14:28 +0800

    fix:优化明暗切换

--- src/hooks/useDark/index.ts
+++ src/hooks/useDark/index.ts
@@ -2,31 +2,32 @@ import { useEffect, useState } from "react";
 
 const useDark = (): [boolean, () => void] => {
     // 从localStorage获取主题，如果没有设置，则默认为'light'
-    const storedTheme = localStorage.getItem('theme') || 'light';
-    const [isDark, setIsDark] = useState<boolean>(storedTheme === 'dark');
+    const storedTheme = localStorage.getItem("theme") || "light";
+    const [isDark, setIsDark] = useState<boolean>(storedTheme === "dark");
+
 
     useEffect(() => {
+        // 根据当前时间自动切换主题
         const time = new Date().getHours();
         if (time >= 18 || time < 6) {
             setIsDark(true);
-            localStorage.setItem('theme', 'dark');
+            localStorage.setItem("theme", "dark");
+            document.body.classList.add("dark");
         } else {
             setIsDark(false);
-            localStorage.setItem('theme', 'light');
+            localStorage.setItem("theme", "light");
+            document.body.classList.add("light");
         }
-        // 应用主题到document.body
-        document.body.classList.remove(isDark ? 'light' : 'dark');
-        document.body.classList.add(isDark ? 'dark' : 'light');
-    }, [isDark]);
+    }, []);
 
     const toggleTheme = () => {
         // 切换主题
-        const newTheme = isDark ? 'light' : 'dark';
-        setIsDark(value => !value);
+        const newTheme = isDark ? "light" : "dark";
+        setIsDark((value) => !value);
         // 存储新主题到localStorage
-        localStorage.setItem('theme', newTheme);
+        localStorage.setItem("theme", newTheme);
         // 应用新主题到document.body
-        document.body.classList.toggle('dark');
+        document.body.classList.toggle("dark");
     };
 
     return [isDark, toggleTheme];
